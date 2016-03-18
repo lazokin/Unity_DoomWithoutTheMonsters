300ae602d60a41ee9417d00eaac8e5da66fc32c3
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-19 08:48:33 +1100

    Keyboard input component: first-person controls

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,10 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MouseLook : MonoBehaviour
-{
-    public enum RotationAxes
-    {
+public class MouseLook : MonoBehaviour {
+
+    public enum RotationAxes {
         MouseXAndY = 0,
         MouseX = 1,
         MouseY = 2
@@ -19,30 +18,22 @@ public class MouseLook : MonoBehaviour
 
     private float _rotationX = 0;
 
-    void Start()
-    {
+    void Start() {
         Rigidbody body = GetComponent<Rigidbody>();
-        if (body != null)
-        {
+        if (body != null) {
             body.freezeRotation = true;
         }
     }
 
-    void Update()
-    {
-	    if (axes == RotationAxes.MouseX)
-        {
+    void Update() {
+	    if (axes == RotationAxes.MouseX) {
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
-        }
-        else if (axes == RotationAxes.MouseY)
-        {
+        } else if (axes == RotationAxes.MouseY) {
             _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
             _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
             float rotationY = transform.localEulerAngles.y;
             transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
-        }
-        else
-        {
+        } else {
             _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
             _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
             float delta = Input.GetAxis("Mouse X") * sensitivityHor;
--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -5,8 +5,7 @@ public class Spin : MonoBehaviour {
 
     public float speed = 3.0f;
 	
-	void Update()
-    {
+	void Update() {
         transform.Rotate(0, speed, 0, Space.World);
 	}
 }
