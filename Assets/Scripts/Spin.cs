ef7056df06386abb0095bc9a37081a9e92742599
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-18 21:17:37 +1100

    Making things move: a script that applies transforms

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+    public float speed = 3.0f;
+	
+	void Update()
+    {
+        transform.Rotate(0, speed, 0, Space.World);
+	}
+}
