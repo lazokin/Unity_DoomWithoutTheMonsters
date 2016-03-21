d17d0c0467a788b2a8b397a31f817027d66eac4d
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-21 17:33:14 +1100

    Shooting via instantiating objects

---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour {
+
+    private int _health;
+
+	void Start () {
+        _health = 5;
+	}
+	
+	public void hurt (int damage) {
+        _health -= damage;
+        Debug.Log("Health: " + _health);
+	}
+}
--- Assets/Scripts/WanderingAI.cs
+++ Assets/Scripts/WanderingAI.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class WanderingAI : MonoBehaviour {
 
+    [SerializeField]
+    private GameObject fireballPrefab;
+    private GameObject _fireball;
+
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
 
@@ -18,7 +22,12 @@ public class WanderingAI : MonoBehaviour {
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
             if (Physics.SphereCast(ray, 0.75f, out hit)) {
-                if (hit.distance < obstacleRange) {
+                GameObject hitObject = hit.transform.gameObject;
+                if (hitObject.GetComponent<PlayerCharacter>()) {
+                    _fireball = Instantiate(fireballPrefab) as GameObject;
+                    _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
+                    _fireball.transform.rotation = transform.rotation;
+                } else if (hit.distance < obstacleRange) {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
                 }
